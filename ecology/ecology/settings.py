f40f5ec574e431e9f24de2f9325fb32c52d388fa
YulianSalo <julian_salo@yahoo.com>
2019-11-14 11:42:06 +0200

    settings applied

--- ecology/ecology/settings.py
+++ ecology/ecology/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'eco',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'ecology.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ecology/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'ecology.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'postgres://sffyzgve:IYod-oqzhk_B7U7zoYfZL54tSoZ7QnaW@manny.db.elephantsql.com:5432/sffyzgve',
+        'OPTIONS': {
+            'read_default_file': 'my.cnf',
+        },
     }
 }
 
